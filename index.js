57cbea8631d100f4f66e5f12953c5ffde17e0b14
coder71 <kamrunnaher877@gmail.com>
2021-06-19 14:45:14 +0600

    exercise 3.13 & 3.14

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
+const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-const app = express()
+const Person = require('./models/person')
+const person = require('./models/person')
 
 //the variable will change when a post request become successfull
 let personDataOnPost
@@ -27,44 +30,24 @@ app.use(morgan(tiny))
 app.use(cors())
 app.use(express.static('build'))
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'May Poppendick',
-    number: '39-23-6423122',
-  },
-]
-
+//get all person data from database
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
   const currentDate = new Date()
-  const data = `<div>Phonebook has info for ${persons.length} people</div>
+  const data = `<div>Phonebook has info for ${Person.length} people</div>
   <div>${currentDate}</div>`
   response.send(data)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find((person) => person.id === id)
-  person ? response.json(person) : response.status(404).end()
+  Person.findById(request.params.id).then((person) => {
+    response.json(person)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -73,32 +56,17 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-//generate a random id for a successful post request data
-const generateId = () => {
-  return Math.random() * 10000000
-}
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  const duplicatePerson = persons.find((person) => person.name === body.name)
 
-  //setting error based on name, number and duplication of name
-  let error = !body.name
-    ? { error: 'please provide name' }
-    : !body.number
-    ? { error: 'please provide number' }
-    : duplicatePerson
-    ? { error: 'name must be unique' }
-    : ''
-
-  if (error) {
-    return response.status(400).json(error)
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'please specify name or number' })
   }
-  const person = {
+
+  const person = new Person({
     name: body.name,
     number: body.number,
-    date: new Date(),
-    id: generateId(),
-  }
+  })
 
   //update the personDataOnPost to show the message in console using morgan middleware
   personDataOnPost = {
@@ -106,11 +74,12 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   }
 
-  persons = persons.concat(person)
-  response.json(person)
+  person.save().then((savedPerson) => {
+    response.json(savedPerson)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
