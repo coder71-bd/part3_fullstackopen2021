2659f5c9d8d081cff85a0e55cad47f4595f62f5f
coder71 <kamrunnaher877@gmail.com>
2021-06-30 13:04:56 +0600

    exercise 3.15

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
-const Person = require('./models/person')
-const person = require('./models/person')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const Person = require('./models/person');
 
 //the variable will change when a post request become successfull
-let personDataOnPost
+let personDataOnPost;
 
 const tiny = (tokens, req, res) => {
   const consoleBody = [
@@ -18,69 +17,74 @@ const tiny = (tokens, req, res) => {
     '-',
     tokens['response-time'](req, res),
     'ms',
-  ]
+  ];
   return tokens.method(req, res) === 'POST'
     ? [...consoleBody, JSON.stringify(personDataOnPost)].join(' ')
-    : [...consoleBody].join(' ')
-}
+    : [...consoleBody].join(' ');
+};
 
 //middlewares
-app.use(express.json())
-app.use(morgan(tiny))
-app.use(cors())
-app.use(express.static('build'))
+app.use(express.json());
+app.use(morgan(tiny));
+app.use(cors());
+app.use(express.static('build'));
 
 //get all person data from database
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
-    response.json(persons)
-  })
-})
+    response.json(persons);
+  });
+});
 
 app.get('/info', (request, response) => {
-  const currentDate = new Date()
+  const currentDate = new Date();
   const data = `<div>Phonebook has info for ${Person.length} people</div>
-  <div>${currentDate}</div>`
-  response.send(data)
-})
+  <div>${currentDate}</div>`;
+  response.send(data);
+});
 
 app.get('/api/persons/:id', (request, response) => {
   Person.findById(request.params.id).then((person) => {
-    response.json(person)
-  })
-})
+    response.json(person);
+  });
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((person) => person.id !== id)
-  response.status(204).end()
-})
+//delete a person from the database
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body
+  const body = request.body;
 
   if (body.name === undefined || body.number === undefined) {
-    return response.status(400).json({ error: 'please specify name or number' })
+    return response
+      .status(400)
+      .json({ error: 'please specify name or number' });
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  })
+  });
 
   //update the personDataOnPost to show the message in console using morgan middleware
   personDataOnPost = {
     name: body.name,
     number: body.number,
-  }
+  };
 
   person.save().then((savedPerson) => {
-    response.json(savedPerson)
-  })
-})
+    response.json(savedPerson);
+  });
+});
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
-})
+  console.log(`server running on port ${PORT}`);
+});
